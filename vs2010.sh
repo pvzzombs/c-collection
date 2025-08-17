df01ab3b8c285f00ce6168bab7284de89161b388
pvzzombs <pvzzombs@gmail.com>
2025-08-17 12:39:06 +0800

    Add more include folder to script

--- vs2010.sh
+++ vs2010.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export INCLUDE="C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include"
+export INCLUDE="C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/include;C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Include"
 export LIB="C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/lib;C:/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Lib"
 export PATH=$PATH:"/c/Program Files (x86)/Microsoft Visual Studio 10.0/Common7/IDE":"/c/Program Files (x86)/Microsoft SDKs/Windows/v7.0A/Bin":"/c/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin"
 
