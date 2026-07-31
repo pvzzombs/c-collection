adea54156eeff9546823db88450725e2d168e06d
pvzzombs <pvzzombs@gmail.com>
2026-07-31 15:58:30 +0800

    Changes:
    - Implement int as key, can be combined with str key
      This will allow key mixing from two different data type
    - Add function forward declarations
    - Add helper functions and new functions

--- ds/hashtable.c
+++ ds/hashtable.c
@@ -6,21 +6,36 @@ int main() {
   HashTable t;
   char a[] = "Wow!";
   int * r_only;
-  int x = 12;
+  char * str;
+  int x = 12, y = 10;
   HashTable_init(&t, 10);
   
   
-  HashTable_insert_str(&t, "1", HASHTABLE_TYPE_STRING, "Wow!");
+  /* HashTable_insert_str(&t, "1", HASHTABLE_TYPE_STRING, "Wow!");
   HashTable_insert_str(&t, "2", HASHTABLE_TYPE_INT, &x);
   HashTable_print(&t);
-  
-  /* HashTable_set_value_from_str(&t, "Noice!!", HASHTABLE_TYPE_STRING, a); */
+
+  HashTable_insert_str(&t, "1", HASHTABLE_TYPE_STRING,"Amazing!");
+  str = (char *) HashTable_get_value_from_str(&t, "1", HASHTABLE_TYPE_STRING);
+  printf("%s\n", str);
+  HashTable_print(&t);
   
   r_only = (int *)HashTable_get_value_from_str(&t, "2", HASHTABLE_TYPE_INT);
   
   printf("%d\n", *r_only);
   
   HashTable_remove_str(&t, "2");
+  HashTable_print(&t); */
+  x = 12;
+  HashTable_insert_int(&t, 100, HASHTABLE_TYPE_INT, &x);
+  HashTable_insert_int(&t, 99, HASHTABLE_TYPE_INT, &x);
+  HashTable_insert_int(&t, 99, HASHTABLE_TYPE_INT, &y);
+  HashTable_print(&t);
+
+  r_only = (int *) HashTable_get_value_from_int(&t, 99, HASHTABLE_TYPE_INT);
+  printf("Num is %d\n", *r_only);
+
+  HashTable_remove_int(&t, 100);
   HashTable_print(&t);
   
   HashTable_destroy(&t);
--- ds/hashtable.h
+++ ds/hashtable.h
@@ -31,38 +31,100 @@ struct HashTable_ {
   HashTable_Entry * table;
 };
 
+void HashTable_entry_list_init(HashTable_Entry_List *);
+void HashTable_entry_init(HashTable_Entry *);
+int HashTable_entry_contains_str(HashTable_Entry *, char *);
+int HashTable_entry_contains_int(HashTable_Entry *, int);
+void HashTable_entry_add_str(HashTable_Entry *, char *, int, void *);
+void HashTable_entry_delete_str(HashTable_Entry *, char *);
+void HashTable_entry_update_str(HashTable_Entry *, char *, int, void *);
+void * HashTable_entry_find_str(HashTable_Entry *, char *, int);
+void HashTable_entry_add_int(HashTable_Entry *, int, int, void *);
+void HashTable_entry_delete_int(HashTable_Entry *, int);
+void HashTable_entry_update_int(HashTable_Entry *, int, int, void *);
+void * HashTable_entry_find_int(HashTable_Entry *, int, int);
+void HashTable_entry_destroy(HashTable_Entry *);
+void HashTable_entry_print(HashTable_Entry *);
+void HashTable_init(HashTable *, int);
+void HashTable_destroy(HashTable *);
+unsigned long HashTable_hash_string_djb2(char *);
+void HashTable_insert_str(HashTable *, char *, int, void *);
+void HashTable_remove_str(HashTable *, char *);
+void HashTable_set_value_from_str(HashTable *, char *, int, void *);
+void * HashTable_get_value_from_str(HashTable *, char *, int);
+void HashTable_print(HashTable *);
+
 #if defined(HASHTABLE_IMPL)
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
+void HashTable_entry_list_init(HashTable_Entry_List * hel) {
+  hel->next = NULL;
+  hel->numKey = 0;
+  hel->numVal = 0;
+  hel->strKey = NULL;
+  hel->strVal = NULL;
+  hel->type = HASHTABLE_TYPE_NONE;
+}
+
 void HashTable_entry_init(HashTable_Entry * he) {
   he->head = NULL;
   he->len = 0;
 }
 
+int HashTable_entry_contains_str(HashTable_Entry * he, char * str) {
+  HashTable_Entry_List * current = he->head;
+  int j = 0;
+  while (current != NULL) {
+    if (strcmp(current->strKey, str) == 0) {
+      return j;
+    }
+    current = current->next;
+    j++;
+  }
+  return -1;
+}
+
+int HashTable_entry_contains_int(HashTable_Entry * he, int key) {
+  HashTable_Entry_List * current = he->head;
+  int j = 0;
+  while (current != NULL) {
+    if (current->numKey == key) {
+      return j;
+    }
+    current = current->next;
+    j++;
+  }
+  return -1;
+}
+
 void HashTable_entry_add_str(HashTable_Entry * he, char * str, int type, void * val) {
   char * key = malloc(sizeof(char) * (strlen(str) + 1));
-  HashTable_Entry_List * newNode = malloc(sizeof(HashTable_Entry_List));
-  strcpy(key, str);
-  newNode->type = HASHTABLE_TYPE_STRING;
-  newNode->strKey = key;
-  newNode->strVal = NULL;
-  if (he->len == 0) {
-    he->head = newNode;
-    he->head->next = NULL;
-    he->len++;
+  if (HashTable_entry_contains_str(he, str) == -1) {
+    HashTable_Entry_List * newNode = malloc(sizeof(HashTable_Entry_List));
+    strcpy(key, str);
+    HashTable_entry_list_init(newNode);
+    newNode->type = HASHTABLE_TYPE_STRING;
+    newNode->strKey = key;
+    if (he->len == 0) {
+      he->head = newNode;
+      he->head->next = NULL;
+      he->len++;
+    } else {
+      newNode->next = he->head;
+      he->head = newNode;
+      he->len++;
+    }
+    if (type == HASHTABLE_TYPE_STRING) {
+      newNode->strVal = malloc(sizeof(char) * (strlen((char*)val) + 1));
+      strcpy(newNode->strVal, (char *)val);
+    } else if (type == HASHTABLE_TYPE_INT) {
+      newNode->numVal = *((int *)val);
+    }
   } else {
-    newNode->next = he->head;
-    he->head = newNode;
-    he->len++;
-  }
-  if (type == HASHTABLE_TYPE_STRING) {
-    newNode->strVal = malloc(sizeof(char) * (strlen((char*)val) + 1));
-    strcpy(newNode->strVal, (char *)val);
-  } else if (type == HASHTABLE_TYPE_INT) {
-    newNode->numVal = *((int *)val);
+    HashTable_entry_update_str(he, str, type, val);
   }
 }
 
@@ -95,7 +157,7 @@ void HashTable_entry_delete_str(HashTable_Entry * he, char * str) {
   }
 }
 
-void HashTable_entry_update_str(HashTable_Entry * he, int type, char * str, void * val) {
+void HashTable_entry_update_str(HashTable_Entry * he, char * str, int type, void * val) {
   HashTable_Entry_List * current = he->head;
   while(current != NULL) {
     if (strcmp(current->strKey, str) == 0) {
@@ -117,7 +179,7 @@ void HashTable_entry_update_str(HashTable_Entry * he, int type, char * str, void
   }
 }
 
-void * HashTable_entry_find_str(HashTable_Entry * he, int type, char * str) {
+void * HashTable_entry_find_str(HashTable_Entry * he, char * str, int type) {
   HashTable_Entry_List * current = he->head;
   while(current != NULL) {
     if (strcmp(current->strKey, str) == 0) {
@@ -138,22 +200,97 @@ void * HashTable_entry_find_str(HashTable_Entry * he, int type, char * str) {
   return NULL;
 }
 
-void HashTable_entry_add_int(HashTable_Entry * he, int num) {
+void HashTable_entry_add_int(HashTable_Entry * he, int num, int type, void * val) {
   int key = num;
-  HashTable_Entry_List * newNode = malloc(sizeof(HashTable_Entry_List));
-  newNode->type = HASHTABLE_TYPE_INT;
-  newNode->numKey = key;
-  newNode->strKey = NULL;
-  newNode->strVal = NULL;
-  if (he->len == 0) {
-    he->head = newNode;
-    he->head->next = NULL;
-    he->len++;
+  if (HashTable_entry_contains_int(he, key) == -1) {
+    HashTable_Entry_List * newNode = malloc(sizeof(HashTable_Entry_List));
+    HashTable_entry_list_init(newNode);
+    newNode->type = HASHTABLE_TYPE_INT;
+    newNode->numKey = key;
+    if (he->len == 0) {
+      he->head = newNode;
+      he->head->next = NULL;
+      he->len++;
+    } else {
+      newNode->next = he->head;
+      he->head = newNode;
+      he->len++;
+    }
   } else {
-    newNode->next = he->head;
-    he->head = newNode;
-    he->len++;
+    HashTable_entry_update_int(he, num, type, val);
   }
+}
+
+void HashTable_entry_delete_int(HashTable_Entry * he, int key) {
+  HashTable_Entry_List * parent = NULL;
+  HashTable_Entry_List * current = he->head;
+  HashTable_Entry_List * next = NULL;
+  while(current != NULL) {
+    if (current->numKey == key) {
+      /* Key found! */
+      next = current->next;
+      break;
+    }
+    parent = current;
+    current = current->next;
+  }
+  
+  if (current != NULL) {
+    if (parent != NULL) {
+      parent->next = next;
+    } else {
+      he->head = next;
+    }
+    if (current->type == HASHTABLE_TYPE_STRING) {
+      free(current->strKey);
+    }
+    free(current->strVal);
+    free(current);
+    he->len--;
+  }
+}
+
+void HashTable_entry_update_int(HashTable_Entry * he, int key, int type, void * val) {
+  HashTable_Entry_List * current = he->head;
+  while(current != NULL) {
+    if (current->numKey == key) {
+      /* Key found! */
+      break;
+    }
+    current = current->next;
+  }
+  
+  if (current != NULL) {
+    if (type == HASHTABLE_TYPE_STRING) {
+      char * s = (char *) val;
+      current->strVal = malloc(sizeof(char) * (strlen(s) + 1));
+      strcpy(current->strVal, s);
+    } else if (type == HASHTABLE_TYPE_INT) {
+      int * num = (int *) val;
+      current->numVal = *num;
+    }
+  }
+}
+
+void * HashTable_entry_find_int(HashTable_Entry * he, int key, int type) {
+  HashTable_Entry_List * current = he->head;
+  while(current != NULL) {
+    if (current->numKey == key) {
+      /* Key found! */
+      break;
+    }
+    current = current->next;
+  }
+  
+  if (current != NULL) {
+    if (type == HASHTABLE_TYPE_STRING) {
+      return (void *) current->strVal;
+    } else if (type == HASHTABLE_TYPE_INT) {
+      return (void *) &(current->numVal);
+    }
+    return NULL;
+  }
+  return NULL;
 }
 
 void HashTable_entry_destroy(HashTable_Entry * he) {
@@ -223,6 +360,16 @@ unsigned long HashTable_hash_string_djb2(char * str) {
   return hash;
 }
 
+unsigned long HashTable_hash_int_djb2(int num) {
+  unsigned long hash = 5381;
+  while (num > 0) {
+    hash = hash * 33 + (num % 10 + '0');
+    num = num / 10;
+  }
+  hash = hash * 33 + '0';
+  return hash;
+}
+
 void HashTable_insert_str(HashTable * h, char * key, int type, void * val) {
   unsigned long i = HashTable_hash_string_djb2(key);
   i = i % h->table_size;
@@ -238,13 +385,37 @@ void HashTable_remove_str(HashTable * h, char * key) {
 void HashTable_set_value_from_str(HashTable * h, char * key, int type, void * val) {
   unsigned long i = HashTable_hash_string_djb2(key);
   i = i % h->table_size;
-  HashTable_entry_update_str(&(h->table[i]), type, key, val);
+  HashTable_entry_update_str(&(h->table[i]), key, type, val);
 }
 
 void * HashTable_get_value_from_str(HashTable * h, char * key, int type) {
   unsigned long i = HashTable_hash_string_djb2(key);
   i = i % h->table_size;
-  return HashTable_entry_find_str(&(h->table[i]), type, key);
+  return HashTable_entry_find_str(&(h->table[i]), key, type);
+}
+
+void HashTable_insert_int(HashTable * h, int key, int type, void * val) {
+  unsigned long i = HashTable_hash_int_djb2(key);
+  i = i % h->table_size;
+  HashTable_entry_add_int(&(h->table[i]), key, type, val);
+}
+
+void HashTable_remove_int(HashTable * h, int key) {
+  unsigned long i = HashTable_hash_int_djb2(key);
+  i = i % h->table_size;
+  HashTable_entry_delete_int(&(h->table[i]), key);
+}
+
+void HashTable_set_value_from_int(HashTable * h, int key, int type, void * val) {
+  unsigned long i = HashTable_hash_int_djb2(key);
+  i = i % h->table_size;
+  HashTable_entry_update_int(&(h->table[i]), key, type, val);
+}
+
+void * HashTable_get_value_from_int(HashTable * h, int key, int type) {
+  unsigned long i = HashTable_hash_int_djb2(key);
+  i = i % h->table_size;
+  return HashTable_entry_find_int(&(h->table[i]), key, type);
 }
 
 void HashTable_print(HashTable * h) {
